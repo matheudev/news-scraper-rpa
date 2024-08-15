be0a60e3d51c7438124442227b6e231f64c2a77b
Matheus <matheudev@gmail.com>
2024-08-15 17:44:41 -0300

    Bug Fix

--- extended_selenium.py
+++ extended_selenium.py
@@ -48,7 +48,7 @@ class ExtendedSelenium(Selenium):
         chrome_options.add_argument("--start-maximized")
         chrome_options.add_argument("--disable-gpu")
         chrome_options.add_argument("--disable-extensions")
-        #chrome_options.add_argument("--headless")
+        chrome_options.add_argument("--headless")
         chrome_options.add_argument("--window-size=1920,1080")
         chrome_options.add_argument('--no-sandbox')
         chrome_options.add_argument('--disable-web-security')
